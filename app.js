1102df43848a28455df58de8bbfe90f14edb9fb8
Ben Tranter <bwtrante@lakeheadu.ca>
2015-06-07 23:16:33 -0400

    Add Backbone view and Snap instance

--- app.js
+++ app.js
@@ -3,5 +3,33 @@ var app = app || {};
 (function() {
   'use strict';
 
-  document.write('Hello, world');
+  var View = Backbone.View.extend({
+    el: 'body',
+
+    // Shortcut to drawing area
+    snap: Snap('#drawingArea'),
+
+    events: {
+      'keypress': 'handleShortcut'
+    },
+
+    initialize: function() {
+      console.log('Hello');
+    },
+
+    render: function() {
+
+    },
+
+    handleShortcut: function(e) {
+      e.preventDefault();
+
+      if (e.keyCode === 114) {
+        console.log('Rect');
+      }
+    }
+  });
+
+  var mainView = new View();
+
 })();
