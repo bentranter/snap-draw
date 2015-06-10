5c2ffe074c736a44f13c74b1648aa6b1707b6378
Ben Tranter <bwtrante@lakeheadu.ca>
2015-06-10 15:07:24 -0400

    Add backbone.localstorage dependency

--- js/models/drawing.js
+++ js/models/drawing.js
@@ -1,6 +1,10 @@
-var Backbone = require('backbone');
+var Backbone     = require('backbone');
+var LocalStorage = require('backbone.localstorage');
 
 module.exports = Backbone.Model.extend({
+  
+  localStorage: new LocalStorage('DrawingModel'),
+
   defaults: {
     title: 'Untitled'
   }
